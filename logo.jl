f90cae53f8bc2dd6bccac7f79ff7ac2bab5c49b5
Alec Loudenback <alecloudenback@gmail.com>
2020-02-26 22:13:05 -0600

    fit boundingbox

--- logo.jl
+++ logo.jl
@@ -6,28 +6,27 @@ const julia_green   = (0.22, 0.596, 0.149)
 const julia_red     = (0.796, 0.235, 0.2)
 
 function draw()
-    currentwidth = 1250 # pts
-    currentheight = 800 # pts
+    currentwidth = 900 # pts
+    currentheight = 250 # pts
     Drawing(currentwidth, currentheight, "logo.svg")
 
     sethue(julia_red)
-    circle(Point(90, 30), 25, :fill)
+    circle(Point(45, 30), 25, :fill)
 
     sethue(julia_purple)
-    circle(Point(165, 30), 25, :fill)
+    circle(Point(120, 30), 25, :fill)
     
     sethue(julia_green)
-    circle(Point(875, 30), 25, :fill)
+    circle(Point(830, 30), 25, :fill)
 
-    origin()
+    # origin()
 
     fontsize(300)                             
     fontface("Literata SemiBold")
     
     str = "actScı" # Uses "dotless i" 
-    w, h = textextents(str)[3:4]              # get width and height
 
-    translate(-(currentwidth/2) + 50, -(currentheight/2) + h)
+    translate(0, 225)
 
     textpath(str)                             # make text into a path
     setline(3)
