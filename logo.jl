2290d14ddf337278e1521fafd6b1fcb91afe7716
Alec Loudenback <alecloudenback@gmail.com>
2020-02-26 22:17:47 -0600

    add square version

--- logo.jl
+++ logo.jl
@@ -5,7 +5,7 @@ const julia_purple  = (0.584, 0.345, 0.698)
 const julia_green   = (0.22, 0.596, 0.149)
 const julia_red     = (0.796, 0.235, 0.2)
 
-function draw(type)
+function draw_full(type)
     currentwidth = 900 # pts
     currentheight = 250 # pts
     Drawing(currentwidth, currentheight, "logo.$type")
@@ -38,4 +38,38 @@ end
 
 draw("png")
 draw("svg")
+preview()
+
+
+function draw_square(type)
+    currentwidth = 250 # pts
+    currentheight = 250 # pts
+    Drawing(currentwidth, currentheight, "logo_square.$type")
+
+    sethue(julia_red)
+    circle(Point(90, 30), 25, :fill)
+
+    sethue(julia_purple)
+    circle(Point(165, 30), 25, :fill)
+
+
+    # origin()
+
+    fontsize(300)                             
+    fontface("Literata SemiBold")
+    
+    str = "a" # Uses "dotless i" 
+
+    translate(45, 225)
+
+    textpath(str)                             # make text into a path
+    setline(3)
+    setcolor("black") 
+    fillpreserve()                            # fill but keep
+
+    finish()
+end
+
+draw_square("png")
+draw_square("svg")
 preview()
